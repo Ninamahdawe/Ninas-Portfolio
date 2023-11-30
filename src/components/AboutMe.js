8ab37a08f8d8ddd6f2e08e094849b96ac242573a
Ninamahdawe <ninamahdawe11@gmail.com>
2023-11-30 01:53:01 -0800

    updating Aboutme.js components file

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,28 +1,15 @@
 
-import React, { useState } from 'react';
-
+import React from 'react';
+import { aboutData } from '../data';
 const AboutMe = () => {
-    const [aboutText, setAboutText] = useState("");
-    const [bio, setBio] = useState("");
+    const { intro, bio, resume } = aboutData;
 
-    seEffect(() => {
-        setAboutText(
-            "Hi, I'm Nina From California. I'm a full stack Software Engineer. I enjoy learning and developing new skills."
-        );
-        setBio(`Passionate in collaborating with cross-functional teams to design develop,
-            and deploy high-quality web applications using both front-end and
-            back-end development technologies, eager to apply academic knowledge
-            and hands-on skills to contribute to my next role.<br /><br />
-            Continuous learner, collaborative communicator, dedicated problem
-            solver and creative thinker`);
-    }, []);
     return (
         <div>
             <h2>About Me</h2>
-            <div>
-                <p dangerouslySetInnerHTML={{ __html: aboutText }} />
-                <p dangerouslySetInnerHTML={{ __html: bio }} />
-            </div>
+            <p>{intro}</p>
+            <p dangerouslySetInnerHTML={{ __html: bio }} />
+            <a href={resume} target="_blank" rel="noopener noreferrer">View my resume</a>
         </div>
     );
 };
