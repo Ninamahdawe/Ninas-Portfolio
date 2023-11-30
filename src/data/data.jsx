c9d24f7c3513f4f984c56f861edabf4d307e35b7
Ninamahdawe <ninamahdawe11@gmail.com>
2023-11-30 01:46:55 -0800

    added the 3 project and my contact information to the data.jsx file

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -14,3 +14,38 @@ export const aboutData = {
   resume:
     "https://drive.google.com/file/d/1XItXY8NzOdqFWNkctN9x52eHPn10t1cu/view?usp=sharing",
 };
+
+export const projectsData = [
+  {
+    title: "The Winners",
+    description:
+      "This project encompasses the development of a comprehensive Task Management Application, addressing the prevalent challenge of effectively organizing and monitoring tasks and projects. It incorporates both front-end and back-end development.",
+    link: "https://shielded-thicket-49496-3fa79d1c89ee.herokuapp.com/",
+  },
+  {
+    title: "Next Text Editor",
+    description:
+      "A Progressive Web App (PWA) text editor for seamless text creation, saving, and installation",
+    link: "https://immense-atoll-35130-70ccb3ac7ff5.herokuapp.com/",
+  },
+  {
+    title: "",
+    description:
+      "This project encompasses the development of a comprehensive Task Management Application, addressing the prevalent challenge of effectively organizing and monitoring tasks and projects. It incorporates both front-end and back-end development.",
+  },
+  {
+    title: "TechTales",
+    description:
+      "communicating about technology is essential for software developers. They often research, document, and share their knowledge and experiences with the broader tech community. The Tech Blog project provides a platform for developers to publish blog posts and engage in discussions with their peers.",
+    link: "https://fast-plateau-85352-b12827c628f9.herokuapp.com/",
+  },
+];
+export const contactData = {
+  linkedin: "https://www.linkedin.com/in/nina-mahdawe-03a966183/",
+  github: "https://github.com/Ninamahdawe",
+};
+export const Footerdata = {
+  copyright: "© 2022 Nina Mahdawe",
+  linkedin: "https://www.linkedin.com/in/nina-mahdawe-03a966183/",
+  github: "https://github.com/Ninamahdawe",
+};
