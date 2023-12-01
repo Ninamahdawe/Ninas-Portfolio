cf2922391468be19f6347648f3621d751f8defbc
Ninamahdawe <ninamahdawe11@gmail.com>
2023-11-30 23:46:32 -0800

    working on my app.jsx file to make the code optm

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import AboutMe from "./pages/AboutMe";
-import Projects from "./pages/Project";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-
 import "./assets/css/index.css";
 
-import { projectsData, aboutData, contactData } from "./data/data.jsx";
+// import AboutMe from "./pages/AboutMe";
+// import Projects from "./pages/Project";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
 
-const router = createBrowserRouter({
-  path: "/",
-  element: <App />,
-  children: [
-    {
-      path: "/about",
-      element: <AboutMe />,
-    },
-    {
-      path: "/projects",
-      element: <Projects data={projectsData} />,
-    },
-    {
-      path: "/contact",
-      element: <Contact data={contactData} />,
-    },
-  ],
-});
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}></RouterProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+// import { projectsData, aboutData, contactData } from "./data/data.jsx";
+
+// const router = createBrowserRouter({
+//   path: "/",
+//   element: <App />,
+//   children: [
+//     {
+//       path: "/about",
+//       element: <AboutMe />,
+//     },
+//     {
+//       path: "/projects",
+//       element: <Projects data={projectsData} />,
+//     },
+//     {
+//       path: "/contact",
+//       element: <Contact data={contactData} />,
+//     },
+//   ],
+// });
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router}></RouterProvider>
+// );
