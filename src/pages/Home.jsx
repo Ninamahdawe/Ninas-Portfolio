ee41c1d0c87dcb79cc90fb9111fece72b0c41b50
Ninamahdawe <ninamahdawe11@gmail.com>
2023-12-01 04:22:01 -0800

    updating my app.jsx and main.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { resumeData } from "../data/data";
+import { Parallax } from "react-parallax";
+import { aboutData, contactData } from "../data/data";
 
 const Home = () => {
   return (
-    <div>
-      <h1>
-        Hi, I'm{" "}
-        <mark style={{ backgroundColor: "lightblue" }}>
-          <strong>Nina Mahdawe</strong>
-        </mark>
-      </h1>
-      <p>Full stack Software Engineer</p>
-      <small>San Jose, California</small>
-      <button>
-        <Link to="/about">Go to About Me</Link>
-      </button>
-      <div>
-        <a href="https://www.linkedin.com">
-          <img src="linkedin_logo.png" alt="LinkedIn Logo" />
-        </a>
-        <a href={resumeData} download>
+    <Parallax bgImage="src/assets/image/Profile-preview.png" strength={500}>
+      <div style={{ height: 500 }}>
+        <h1>
+          Hi, I'm{" "}
+          <mark style={{ backgroundColor: "lightblue" }}>
+            <strong>{aboutData.name}</strong>
+          </mark>
+        </h1>
+        <p>{aboutData.occupation}</p>
+        <small>{aboutData.location}</small>
+        <button>
+          <Link to="/about">Go to About Me</Link>
+        </button>
+        <a href={aboutData.resume} download>
           Download Resume
         </a>
+        <button>
+          <a href={contactData.linkedin}>LinkedIn</a>
+        </button>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
