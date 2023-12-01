fbae1ba878e7124d6c460f7ab1e60a8d23c433e6
Ninamahdawe <ninamahdawe11@gmail.com>
2023-11-30 23:02:58 -0800

    working on my app.jsx file to make the code optm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { ContactForm, Footer, Nav, Hero } from "./components";
+import { ContactForm, Footer, Nav, Hero, StarsCanvas } from "./components";
+import AboutMe from "./pages/AboutMe";
 // import Nav from "../src/components/Nav.jsx";
 // import Home from "../src/pages/Home.jsx";
 // import About from "../src/pages/AboutMe.jsx";
@@ -13,17 +14,23 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <Nav />
-        <Hero />
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Nav />
+          <Hero />
+        </div>
+        <AboutMe />
+        <ContactForm />
+        <Footer />
+        <div className="relative z-0">
+          <StarsCanvas />
+        </div>
       </div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-      <Footer data={footerData} />
     </BrowserRouter>
   );
 };
-
 export default App;
+
+/* <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} /> */
+/* <Route path="/contact" component={Contact} /> */
+/* <Footer data={footerData} /> */
