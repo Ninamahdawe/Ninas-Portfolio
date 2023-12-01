a9fe7ecf8fc4d661af45d75ad1a27e74a2163d6a
Ninamahdawe <ninamahdawe11@gmail.com>
2023-12-01 03:50:54 -0800

    updating my app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ContactForm from "./components/ContactForm";
-import AboutMe from "./pages/AboutMe.jsx";
-import Project from "./pages/Project.jsx";
-import Contact from "./pages/Contact.jsx";
-import Home from "./pages/Home.jsx";
-import { aboutData, projectsData, contactData } from "./data/data.jsx";
+import Footer from "./components/Footers";
+import NavBar from "./components/NavBar";
 
-const App = () => {
+function App() {
   return (
     <>
-      <Navbar />
-      <Home data={aboutData} />
-      <Routes>
-        <Route path="/about" element={<AboutMe data={aboutData} />} />
-        <Route path="/portfolio" element={<Project data={projectsData} />} />
-        <Route path="/contact" element={<Contact data={contactData} />} />
-      </Routes>
+      <NavBar />
+      <Outlet />
       <ContactForm />
       <Footer />
     </>
   );
-};
+}
 
 export default App;
-
-// import AboutMe from "./components/AboutMe";
-// import Nav from "../src/components/Nav.jsx";
-// import Home from "../src/pages/Home.jsx";
-// import About from "../src/pages/AboutMe.jsx";
-// import Contact from "./components/ContactForm.jsx";
-// import Footer from "./components/Footer.jsx";
-// import { Footerdata } from "./data/data.jsx";
-// import "./assets/css/App.css";
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <div className="relative z-0 bg-primary">
-//         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-//           <Nav />
-//           <Hero />
-//         </div>
-//         <AboutMe />
-//         <Project />
-//         <Contact />
-//         <ContactForm />
-//         <Footer />
-//         <div className="relative z-0">
-//           <StarsCanvas />
-//         </div>
-//       </div>
-//     </BrowserRouter>
-//   );
-// };
-// export default App;
-
-/* <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} /> */
-/* <Route path="/contact" component={Contact} /> */
-/* <Footer data={footerData} /> */
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutMe from "./pages/AboutMe.jsx";
+import Project from "./pages/Project.jsx";
+import Contact from "./pages/Contact.jsx";
+import Home from "./pages/Home.jsx";
 import App from "./App";
 
-// import AboutMe from "./pages/AboutMe";
-// import Projects from "./pages/Project";
-// import Contact from "./pages/Contact";
-// import Home from "./pages/Home";
+import { aboutData, projectsData, contactData } from "./data/data.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <AboutMe data={aboutData} />,
+      },
+      {
+        path: "projects",
+        element: <Project data={projectsData} />,
+      },
+      {
+        path: "contact",
+        element: <Contact data={contactData} />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
+  <RouterProvider router={router} />
 );
